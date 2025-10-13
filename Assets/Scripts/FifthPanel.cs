0e85567260536be2f86c90d4c880ac71daa66d43
Steven Coghill <scoghill1955@gmail.com>
2025-10-13 14:36:22 -0400

    Add PopTo method

--- Assets/Scripts/FifthPanel.cs
+++ Assets/Scripts/FifthPanel.cs
@@ -6,10 +6,13 @@ public class FifthPanel : MonoBehaviour
     [SerializeReference] PanelManager panelManager;
     [SerializeReference] Button prevBtn;
     [SerializeReference] Button popBtn;
+    [SerializeReference] Button popToBtn;
+    [SerializeReference] Panel toPanel;
 
     void Start()
     {
         prevBtn.onClick.AddListener(() => panelManager.Pop());
         popBtn.onClick.AddListener(() => panelManager.Pop(2));
+        popToBtn.onClick.AddListener(() => panelManager.PopTo(toPanel));
     }
 }
